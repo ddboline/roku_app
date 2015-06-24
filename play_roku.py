68414fa1ea2c3bed78a2e77bcb0c0718a23e2da7
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-24 16:48:17 -0400

    new

---
+++ play_roku.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from roku_app.roku_utils import play_roku
+
+if __name__ == '__main__':
+    play_roku()
---
+++ run_remove_commercials.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+'''
+    Script to record from roku device via WinTV HVR-1950
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+from roku_app.remove_commercials import remove_commercials
+
+if __name__ == '__main__':
+    INFILE = os.sys.argv[1]
+    OUTFILE = INFILE.split('/')[-1]
+    if len(os.sys.argv) > 2:
+        OUTFILE = os.sys.argv[2]
+    if len(os.sys.argv) > 3:
+        TIMING_FILENAME = os.sys.argv[3]
+    TIMING_STRING = open(TIMING_FILENAME, 'r').read()
+
+
+    remove_commercials(INFILE, OUTFILE, TIMING_STRING)
