9a4d3e60c3c49f022f0f6aaf59176b97cb941078
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-26 17:26:41 -0400

    pylint

--- send_to_roku.py
+++ send_to_roku.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+'''
+    script to send commands to roku
+'''
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -11,15 +15,15 @@ from roku_app.util import run_command
 from roku_app.roku_utils import send_to_roku, HOMEDIR
 
 if __name__ == '__main__':
-    hostname = os.uname()[1]
-    remotehost = 'ddbolineathome.mooo.com'
+    HOSTNAME = os.uname()[1]
+    REMOTEHOST = 'ddbolineathome.mooo.com'
 
-    if hostname != 'dilepton-tower':
-        _cmd = 'python %s/scripts/send_to_roku.py' % HOMEDIR
+    if HOSTNAME != 'dilepton-tower':
+        CMD = 'python %s/scripts/send_to_roku.py' % HOMEDIR
         for arg in os.sys.argv[1:]:
-            _cmd = '%s %s' % (_cmd, arg)
-        run_command('ssh ddboline@%s \"%s\"' % (remotehost, _cmd))
+            CMD = '%s %s' % (CMD, arg)
+        run_command('ssh ddboline@%s \"%s\"' % (REMOTEHOST, CMD))
     else:
-        retval = send_to_roku(os.sys.argv[1:])
-        if len(retval) > 0 and 'command' not in retval:
-            print(retval.strip())
+        RETVAL = send_to_roku(os.sys.argv[1:])
+        if len(RETVAL) > 0 and 'command' not in RETVAL:
+            print(RETVAL.strip())
