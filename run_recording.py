6a9456b5331acb5083eafdbf5c94d03729f20458
Daniel Boline <ddboline@gmail.com>
2015-08-20 10:16:11 -0400

    cleanup

--- run_recording.py
+++ run_recording.py
@@ -7,7 +7,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
+from sys import argv
 from roku_app.record_roku import record_roku
 
 if __name__ == '__main__':
@@ -15,17 +15,15 @@ if __name__ == '__main__':
     RECORDING_TIME = 3600
     DO_FIX_PVR = False
 
-    if len(os.sys.argv) > 1:
-        RECORDING_NAME = os.sys.argv[1]
-    if len(os.sys.argv) > 2:
+    if len(argv) > 1:
+        RECORDING_NAME = argv[1]
+    if len(argv) > 2:
         try:
-            RECORDING_TIME = int(os.sys.argv[2])*60
+            RECORDING_TIME = int(argv[2])*60
         except ValueError:
             pass
-    if len(os.sys.argv) > 3:
-        if os.sys.argv[3] == 'fix_pvr':
+    if len(argv) > 3:
+        if argv[3] == 'fix_pvr':
             DO_FIX_PVR = True
 
-    record_roku(recording_name=RECORDING_NAME,
-                             recording_time=RECORDING_TIME,
-                             do_fix_pvr=DO_FIX_PVR)
+    record_roku(RECORDING_NAME, RECORDING_TIME, DO_FIX_PVR)
