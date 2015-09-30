6c62f57c13236591a6a2c83235d1dda0500a2362
Daniel Boline <ddboline@gmail.com>
2015-09-30 19:34:58 -0400

    ...
    Signed-off-by: Daniel Boline <ddboline@gmail.com>

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 nosetests --with-coverage --cover-package=roku_app roku_app/*.py
+
+# pyreverse roku_app
+# for N in classes packages; do dot -Tps ${N}*.dot > ${N}.ps ; done
+
 ./roku_app/get_dev.py pvrusb2
