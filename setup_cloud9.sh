3445248967e4612c4aad23081c0392454ae7d895
Daniel Boline <ddboline@gmail.com>
2015-11-27 09:10:15 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y python-numpy python-scipy python-dateutil python-requests python-matplotlib python-nose\
-                        mplayer mpv mencoder python-coverage
+                        mplayer mpv mencoder python-coverage python-setuptools python-dev
 
 mkdir -p ${HOME}/netflix/jobs ${HOME}/netflix/avi ${HOME}/netflix/log ${HOME}/netflix/mpg ${HOME}/netflix/tmp
