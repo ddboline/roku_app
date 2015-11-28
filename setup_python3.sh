df2db6184ba462bf739c26f565e440e2a475c5be
Daniel Boline <ddboline@gmail.com>
2015-11-28 12:29:23 -0500

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -5,7 +5,9 @@ export LANG="C.UTF-8"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y python3-numpy=1.\* python3-scipy python3-dateutil python3-requests python3-matplotlib python3-nose\
-                        mplayer mpv mencoder python3-coverage python3-setuptools python3-dev
+sudo apt-get install -y --force-yes python3-numpy=1.\* python3-scipy python3-dateutil \
+                                    python3-requests python3-matplotlib python3-nose\
+                                    mplayer mpv mencoder python3-coverage \
+                                    python3-setuptools python3-dev
 
 mkdir -p ${HOME}/netflix/jobs ${HOME}/netflix/avi ${HOME}/netflix/log ${HOME}/netflix/mpg ${HOME}/netflix/tmp
