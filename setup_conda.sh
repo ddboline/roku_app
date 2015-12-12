6deef3ef7cbd9bdf49582cdb76aa056e9ccbbe09
Daniel Boline <ddboline@gmail.com>
2015-12-12 18:56:06 -0500

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes \
-    numpy scipy dateutil requests matplotlib nose coverage
-
-sudo apt-get install -y mplayer mpv mencoder numpy scipydateutil requests matplotlib nose coverage
+    numpy scipy python-dateutil requests matplotlib nose coverage
 
 mkdir -p ${HOME}/netflix/jobs ${HOME}/netflix/avi ${HOME}/netflix/log ${HOME}/netflix/mpg ${HOME}/netflix/tmp
