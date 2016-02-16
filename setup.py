6bd96e358f7b4587b4735108d7c2366b2675b0ca
Daniel Boline <ddboline@gmail.com>
2016-02-16 08:07:33 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='roku_app',
-    version='0.0.2.4',
+    version='0.0.2.5',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='roku_app',
