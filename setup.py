be0430b24d8b162c53199eb27e0da8ee60fdd366
Daniel Boline <ddboline@gmail.com>
2016-05-04 11:55:04 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='roku_app',
-    version='0.0.2.9',
+    version='0.0.3.0',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='roku_app',
@@ -22,5 +22,6 @@ setup(
     package_dir={'roku_app': 'roku_app'},
     package_data={'roku_app': ['roku_app/templates/*.html', ]},
     entry_points={'console_scripts':
-                  ['run-recording = roku_app.record_roku:main', ]}
+                  ['run-recording = roku_app.record_roku:main', 
+                   'run-remcom-test = roku_app.remcom:remcom_test_main']}
 )
