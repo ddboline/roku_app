540c302875d07de7efdbcfc679a2e184be91e948
Daniel Boline <ddboline@gmail.com>
2016-12-12 06:56:47 -0500

    run_encoding

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='roku_app',
-    version='0.0.4.5',
+    version='0.0.4.6',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='roku_app',
@@ -22,6 +22,7 @@ setup(
     package_data={'roku_app': ['roku_app/templates/*.html', ]},
     entry_points={'console_scripts':
                   ['run-recording = roku_app.record_roku:main',
+                   'run-encoding = roku_app.run_encoding:run_encoding',
                    'run-remcom-test = roku_app.remcom:remcom_test_main',
                    'run-remove-commercials = roku_app.remove_commercials:main',
                    'play-roku = roku_app.roku_utils:play_roku',
