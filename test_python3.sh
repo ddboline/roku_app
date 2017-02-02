09a6e08a86455bc35a780a3c52adcb694c0e415b
Daniel Boline <ddboline@gmail.com>
2017-02-02 16:19:39 -0500

    ...

--- test_python3.sh
+++ test_python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-py.test --cov=roku_app roku_app/*.py
+py.test3 --cov=roku_app roku_app/*.py
 
 # pyreverse roku_app
 # for N in classes packages; do dot -Tps ${N}*.dot > ${N}.ps ; done
