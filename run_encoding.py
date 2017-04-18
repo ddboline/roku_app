c3d0016eaa1e2d3eac31f2966a1a14aa7ded6a88
Daniel Boline <ddboline@gmail.com>
2017-04-18 09:34:34 -0400

    ...

--- run_encoding.py
+++ run_encoding.py
@@ -2,12 +2,15 @@
 '''
     Script to record from roku device via WinTV HVR-1950
 '''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from time import sleep
 
 from roku_app.run_encoding import run_encoding
 
 if __name__ == '__main__':
-    run_encoding()
+    try:
+        run_encoding()
+    except Exception as exc:
+        print('Caught exception %s' % exc)
+        sleep(10)
