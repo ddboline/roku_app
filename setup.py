956d667359bcef986feffb3b93af454cc11b9734
Daniel Boline <ddboline@gmail.com>
2017-05-26 22:16:16 -0400

    ...

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ else:
 
 setup(
     name='roku_app',
-    version='0.0.5.3',
+    version='0.0.5.4',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='roku_app',
